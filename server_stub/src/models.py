77de8ae3dc3c8d07746eeb0ad611ac0f7086a725
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2010-10-11 19:28:07 +0200

    Removed guck

--- server_stub/src/models.py
+++ server_stub/src/models.py
@@ -23,14 +23,3 @@ class Message(models.Model):
     class Meta:
         db_table = 'messages'
         app_label = "flow"
-
-
-if __name__ == "__main__":
-    import database
-    database.Database()
-    u1 = User.objects.create(name="Brissmyr")
-    u2 = User.objects.create(name="Gyllen")
-    Message.objects.create(user=u1, body="Hallå, alla!")
-    Message.objects.create(user=u2, body="Tjena grabben. Allt väl?")
-    print User.objects.all()
-    print Message.objects.all()
